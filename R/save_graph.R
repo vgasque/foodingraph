7cb8823c32f9704ade520b4b9f43ce82b61f19fb
Victor Gasque <victor.gasque@protonmail.com>
2019-08-29 18:51:16 +0200

    Add compare_graph

--- R/save_graph.R
+++ R/save_graph.R
@@ -1,11 +1,14 @@
 #' Save graph
 #'
-#' Save the graph generated from \code{graph_from_matrix()} or \code{graph_from_links_nodes()}.
+#' Save the graph generated from \code{graph_from_matrix()} or \code{graph_from_links_nodes()}
+#' or \code{compare_graphs}.
 #'
 #' @param graph : the graph
 #' @param filename (optional) : the name of the file. Default is the igraph unique ID in a .png format
-#' @param width (optional) : width of the image in cm. Default is 25 cm
-#' @param height (optional) : height of the image in cm. Default is 20 cm
+#' @param width (optional) : width of the image in cm. Default is 25 cm for a single graph or
+#' a comparison in a vertical position. For a comparison in an horizontal position, 40cm.
+#' @param height (optional) : height of the image in cm. Default is 20 cm for a single graph, 25cm for
+#' a comparison in an horizontal position.For a comparison in a vertical position, 40cm.
 #' @param dpi (optional) : the resolution of the image in dpi. Default is 300
 #' @param ... : other parameters to pass to the \code{ggsave} ggplot2 function
 #' @examples
@@ -19,13 +22,26 @@
 #' @export
 save_graph <- function(graph,
                        filename = NULL,
-                       width = 25,
-                       height = 20,
+                       width = NULL,
+                       height = NULL,
                        dpi = 300,
                        ...) {
 
   if (is.null(graph$igraph) || is.null(graph$net)) {
     stop("Use graphs created by graph_from_matrix() or graph_from_links_nodes(). Or use ggsave()")
+  }
+
+  if (is.null(width) || is.null(height)) {
+    if (inherits(graph, "foodingraph_vertical")) {
+      width <- 25
+      height <- 40
+    } else if (inherits(graph, "foodingraph_horizontal")) {
+      width <- 40
+      height <- 25
+    } else {
+      width <- 25
+      height <- 20
+    }
   }
 
   # Is filename is not given, uses the igraph ID
